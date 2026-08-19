c30e91be447a04a97930d8a61045cd647ca4eabb
monasr6600-png <monasr6600@gmail.com>
2026-08-19 19:55:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+console.log("Mohamed Omar Portfolio Loaded Successfully!");
 
-    console.log("Mohamed Omar Portfolio Loaded Successfully");
+const links = document.querySelectorAll("nav a");
 
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        console.log("Navigation:", link.textContent);
+    });
 });
